9486029ad7c1084093c3e22f1083fec9c6d85bf3
nyxfor13days <42898843+nyxfor13days@users.noreply.github.com>
2023-02-25 17:09:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import RootRouter from "./routes";
 import { Suspense } from "react";
+import { GlobalStyles } from "./theme";
 
 const App = () => {
   return (
     <div>
+      <GlobalStyles />
       <BrowserRouter>
         <Suspense fallback={<div>Loading...</div>}>
           <RootRouter />
