273a8660431a07b63543f00e0f15b8c7e4622821
nyxfor13days <42898843+nyxfor13days@users.noreply.github.com>
2023-02-25 16:34:22 +0530

    Setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import RootRouter from "./routes";
+import { Suspense } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <RootRouter />
+        </Suspense>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
